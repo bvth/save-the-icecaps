1c75013162d4383e4667cee1f04d869c6ce9ae28
thanh.bui1995@gmail.com <thanh.bui1995@gmail.com>
2021-04-29 22:02:00 +0300

    initiate project

---
+++ webpack.dev.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const webpack = require("webpack");
+const srcDir = path.resolve(__dirname, "./src");
+
+module.exports = {
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		path: path.resolve(__dirname, "./public"),
+		filename: "bundle.js"
+	},
+	devtool: "eval-source-map",
+	devServer: {
+		contentBase: path.resolve(__dirname, "./public"),
+		compress: false,
+		port: 9000,
+		hot: true
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	],
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				include: srcDir,
+				use: {
+					loader: "babel-loader",
+					options: {
+						sourceMaps: true,
+						presets: ["@babel/preset-env", "@babel/preset-react"]
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			}
+		]
+	}
+};
